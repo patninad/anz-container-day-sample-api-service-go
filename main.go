e43ea8bd2f86fe61ccc685e20fa0037a208b8ab0
Ninad Patil <patninad@gmail.com>
2020-01-31 12:32:40 +1100

    Update main.go

--- main.go
+++ main.go
@@ -33,7 +33,7 @@ func main() {
 
 		io.WriteString(w, string(out))
 
-		fmt.Println("Hello world - the log message")
+		fmt.Println("Hello world Ninad - the log message")
 	})
 	http.ListenAndServe(":8080", nil)
 }
